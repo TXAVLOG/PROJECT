776f7139510341f38107ae33bc1d5f7c3e1c4a81
TXAVLOG <viptretrauc@gmail.com>
2025-12-21 18:38:47 +0700

    ME

--- app/src/main/java/gc/txa/demo/ui/TXASettingsActivity.kt
+++ app/src/main/java/gc/txa/demo/ui/TXASettingsActivity.kt
@@ -1,6 +1,7 @@
 package gc.txa.demo.ui
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
