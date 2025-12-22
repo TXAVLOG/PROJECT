4b57755fe831bfa6ce38fa71b24a52d33e1a5573
TXAVLOG <viptretrauc@gmail.com>
2025-12-22 21:32:48 +0700

    fix: rename TXA Music + Hilt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
-    id("dagger.hilt.android.plugin") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
